cfab88a98970833711c6f63dc05a28d5b5189dc8
BORI <88658551+Kbomi16@users.noreply.github.com>
2024-06-13 16:24:50 +0900

    📝 6회차_[2장]_보미 (#26)
    * 📝 6회차_[2장]_보미
    
    * 📝 docs: 렌더, 커밋 내용 보충
    
    * 🎉 실습 폴더 생성
    
    * ✨ 생명주기 비교
    
    * 🎉
    
    * 🎉

---
+++ 실습/lifecycle/src/App.jsx
@@ -0,0 +1,14 @@
+import LifeCycleExample from '../components/LifeCycleExample'
+import LifeCycleHooksExample from '../components/LifeCycleHooksExample'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <LifeCycleExample />
+      <LifeCycleHooksExample />
+    </>
+  )
+}
+
+export default App
