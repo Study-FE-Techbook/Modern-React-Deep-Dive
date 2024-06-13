d17727c2da1942fbcd4530cc37176ab2d9bca26f
alexgoni <goni000211@gmail.com>
2024-06-13 18:16:12 +0900

    feat: rendering 실습 파일 추가

---
+++ 실습/rendering/App.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export default function App() {
+  debugger;
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    setTimeout(() => {
+      setData({
+        hello: "world",
+      });
+
+      setLoading(false);
+    }, 3_000);
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+
+  return <>{data ? JSON.stringify(data) : <div>first render</div>}</>;
+}
