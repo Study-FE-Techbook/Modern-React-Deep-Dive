252283a935d0b65f81099800aed2d51ffeac87b5
kimminjae <162538553+PixeIDark@users.noreply.github.com>
2024-07-01 16:00:15 +0900

    📝 11회차_[6장]_민재 (#60)

--- 실습/민재/src/App.jsx
+++ 실습/민재/src/App.jsx
@@ -1,9 +1,10 @@
 import Counter from "./components/Counter";
-
+import AnonymousDefaultComponent from "./components/devToolTest/NoNamed";
 function App() {
   return (
     <>
       <Counter />
+      <AnonymousDefaultComponent />
     </>
   );
 }
---
+++ 실습/민재/src/components/devToolTest/NoNamed.tsx
@@ -0,0 +1,5 @@
+const A = () => {
+  return <div>noNamedComponent</div>;
+};
+
+A.displayName = "컴포넌트 A!";
