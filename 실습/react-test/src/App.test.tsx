2bdc10314bff8e2a5f2b4c03ab6f62da52a5bb7d
김보미 <kimbomi2172@naver.com>
2024-07-10 21:00:05 +0900

    📝 docs: 참고 추가

--- 실습/react-test/src/App.test.tsx
+++ 실습/react-test/src/App.test.tsx
@@ -11,3 +11,4 @@ test('renders learn react link', () => {
 // 1. App 렌더링
 // 2. 렌더링하는 컴포넌트 내부에서 "learn react"라는 문자열을 가진 DOM 요소 찾기
 // 3. expect(linkElement).toBeInTheDocument()라는 어설션을 활용해 2번에서 찾은 요소가 document 내부에 있는지 확인한다.
+// npm test -- --testPathPattern=src/App.test.tsx
