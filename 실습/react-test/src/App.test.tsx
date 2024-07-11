926dd6b28ad35ee1acceece9d2934042025f91f4
BORI <88658551+Kbomi16@users.noreply.github.com>
2024-07-11 18:48:16 +0900

    📝 13회차_[8장]_보미 (#69)
    * 🎉 실습 코드 추가
    
    * 📝 13회차_[8장]_보미
    
    * 📝 docs: 참고 추가

---
+++ 실습/react-test/src/App.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react'
+
+import App from './App'
+
+test('renders learn react link', () => {
+  render(<App />)
+  const linkElement = screen.getByText(/learn react/i)
+  expect(linkElement).toBeInTheDocument()
+})
+
+// 1. App 렌더링
+// 2. 렌더링하는 컴포넌트 내부에서 "learn react"라는 문자열을 가진 DOM 요소 찾기
+// 3. expect(linkElement).toBeInTheDocument()라는 어설션을 활용해 2번에서 찾은 요소가 document 내부에 있는지 확인한다.
+// npm test -- --testPathPattern=src/App.test.tsx
